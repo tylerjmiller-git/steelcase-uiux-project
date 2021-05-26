6baf38533faeade7de9a8c7608f0529caae5466d
tylerjmiller-git <84867548+tylerjmiller-git@users.noreply.github.com>
2021-05-26 11:06:00 -0600

    code for review

---
+++ steelcase-handlebar-data.js
@@ -0,0 +1,61 @@
+var dataSource = {
+    "products": [
+      {
+        "name": "Greenpoint Private Desk",
+        "brand": "West Elm",
+        "price": "1036",
+        "currency": "USD",
+        "image": "https://via.placeholder.com/269x269.png?%20C/O%20https://placeholder.com/"
+      },
+      {
+        "name": "Greenpoint Mobile Pedestal",
+        "brand": "West Elm",
+        "price": "1036",
+        "currency": "USD",
+        "image": "https://via.placeholder.com/269x269.png?%20C/O%20https://placeholder.com/"
+      },
+      {
+        "name": "Greenpoint Lateral File",
+        "brand": "West Elm",
+        "price": "1036",
+        "currency": "USD",
+        "image": "https://via.placeholder.com/269x269.png?%20C/O%20https://placeholder.com/"
+      },
+      {
+        "name": "Greenpoint Credenza",
+        "brand": "West Elm",
+        "price": "1036",
+        "currency": "USD",
+        "image": "https://via.placeholder.com/269x269.png?%20C/O%20https://placeholder.com/"
+      },
+      {
+        "name": "Slope Guest Chair",
+        "brand": "West Elm",
+        "price": "1036",
+        "currency": "USD",
+        "image": "https://via.placeholder.com/269x269.png?%20C/O%20https://placeholder.com/"
+      },
+      {
+        "name": "Slope Lounge Chair",
+        "brand": "West Elm",
+        "price": "1036",
+        "currency": "USD",
+        "image": "https://via.placeholder.com/269x269.png?%20C/O%20https://placeholder.com/"
+      },
+      {
+        "name": "Sterling Conference Chair",
+        "brand": "West Elm",
+        "price": "1036",
+        "currency": "USD",
+        "image": "https://via.placeholder.com/269x269.png?%20C/O%20https://placeholder.com/"
+      },
+      {
+        "name": "Sterling Guest Chair",
+        "brand": "West Elm",
+        "price": "1036",
+        "currency": "USD",
+        "image": "https://via.placeholder.com/269x269.png?%20C/O%20https://placeholder.com/"
+      }
+    ]
+  }
+  
